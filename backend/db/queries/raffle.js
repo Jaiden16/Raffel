0238b786ecb550b93a5100e0b147c31fec4d4124
jonathan <jonathanfagan@pursuit.org>
2021-05-30 19:48:58 -0400

    get participants and post participants routes completed

--- backend/db/queries/raffle.js
+++ backend/db/queries/raffle.js
@@ -4,7 +4,7 @@ const getRaffles = async (req, res, next) => {
     try {
         const raffle = await db.any("SELECT * FROM raffles");
         res.json({
-            raffle,
+            data: raffle,
             message: 'success'
         }).status(200)
 
@@ -16,8 +16,8 @@ const getRaffles = async (req, res, next) => {
 
 const postRaffle = async (req, res, next) => {
     let info = {};
-    let query = 
-    "INSERT INTO raffles(name,secret_token,created_at) VALUES (${name},${secret},${time}) RETURNING *"
+    let query =
+        "INSERT INTO raffles(name,secret_token,created_at) VALUES (${name},${secret},${time}) RETURNING *"
     let date = new Date();
     // console.log(date.toISOString())
 
@@ -33,9 +33,7 @@ const postRaffle = async (req, res, next) => {
         if (req.body.name && req.body.secret) {
             info.time = date.toISOString()
 
-
-
-            let payload = await db.one(query,info)
+            let payload = await db.one(query, info)
             console.log(payload);
             res.json({
                 data: payload,
@@ -46,10 +44,81 @@ const postRaffle = async (req, res, next) => {
 
     } catch (err) {
         console.log(err)
+    }
+}
+
+//get /raffles/:id - retrieve a single raffle by its id
+const getSingleRaffle = async (req, res, next) => {
+    try {
+        let raffle = await db.oneOrNone(`SELECT * FROM raffles where id = ${req.params.id} `)
+        res.json({
+            data: raffle,
+            message: 'retrieved raffle'
+        }).status(200)
+
+    } catch (err) {
+        console.log(err)
 
     }
+}
 
+
+
+
+
+//get /raffles/:id/participants retrieve all participants of a raffle
+const getRaffleParticipants = async(req,res,next) =>{
+    let query = `SELECT firstname,lastname,email,phone,registered_at FROM raffles 
+    JOIN users ON (users.raffle_id = ${req.params.id})`
+    try{
+        let users = await db.any(query)
+        res.status(200).json({
+            data: users,
+            message: 'success'
+        })
+
+    }catch(err){
+        console.log(err)
+    }
 
 }
 
-module.exports = { getRaffles, postRaffle };
+//post /raffles/:id/participants sign up a participant to a raffle given a raffle id.
+const postParticipant = async (req, res, next) => {
+    let info = {};
+    let date = new Date();
+    let query = 'INSERT into users(raffle_id,firstname,lastname,email,phone,registered_at)'
+    let values = 'VALUES(${id},${first},${last},${email},${phone},${registered})';
+    let fullQuery = query + values + 'RETURNING *';
+    try {
+        if (req.body.first && req.body.last && req.body.email) {
+            info.id = req.params.id
+            info.first = req.body.first,
+            info.last = req.body.last,
+            info.email = req.body.email
+        }
+
+        if (req.body.phone) {
+            info.phone = req.body.phone
+            info.registered = date.toISOString()
+        } else {
+            info.phone = "n/a"
+            info.registered = date.toISOString()
+
+        }
+
+        let payload = await db.one(fullQuery, info)
+        res.json({
+            data: payload,
+            message: 'success'
+        }).status(200)
+    } catch (err) {
+        res.json({
+            data: "Email already used"
+        })
+        console.log(err)
+    }
+}
+
+
+module.exports = { getRaffles, postRaffle, getSingleRaffle,getRaffleParticipants, postParticipant };
--- backend/routes/raffles.js
+++ backend/routes/raffles.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { get } = require('.');
 const router = express.Router();
-const {getRaffles, postRaffle} = require("../db/queries/raffle");
+const {getRaffles, postRaffle, getSingleRaffle, postParticipant, getRaffleParticipants} = require("../db/queries/raffle");
 
 
 router.get('/raffles',getRaffles);
-router.post('/raffles',postRaffle)
+router.post('/raffles',postRaffle);
+router.get('/raffles/:id',getSingleRaffle)
+router.post('/raffles/:id/participants',postParticipant)
+router.get('/raffles/:id/participants',getRaffleParticipants)
 
 module.exports = router;
